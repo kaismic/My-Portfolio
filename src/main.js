151b5d0d31b8e8b349ee742e718660f568cb0c2e
Isac Kim <isac08270@gmail.com>
2024-04-23 00:13:41 +1000

    added skills item width adjusting code

--- src/main.js
+++ src/main.js
@@ -21,3 +21,41 @@ Array.from(document.getElementsByClassName('embla__carousel')).map(
         return api
     }
 )
+
+let count = 0
+
+Array.from(document.getElementsByClassName('project__desc-grid')).map(
+    (descGridNode, idx, arr) => {
+        const skillsItemResizeObserver = new ResizeObserver(
+            function (entries, observer) {
+                const skillsContainerWidth = descGridNode.querySelector('.project__skills-container').offsetWidth
+                Array.from(descGridNode.getElementsByClassName('project__skills-item')).map(
+                    /**
+                     * 
+                     * @param {HTMLElement} spanTextNode 
+                     * @param {number} idx 
+                     * @param {Array<HTMLElement>} arr 
+                     */
+                    (spanTextNode, idx, arr) => {
+                        spanTextNode.style.textWrap = "nowrap"
+                        /**
+                         * @type {HTMLDivElement}
+                         */
+                        const pn = spanTextNode.parentNode
+                        const pnStyleMap = pn.computedStyleMap()
+                        // if the text full width is less than container width, keep text-wrap nowrap and remove width property in case it was assigned before
+                        if (spanTextNode.offsetWidth <= skillsContainerWidth) {
+                            pn.style.removeProperty("width")
+                        }
+                        // the text full width (single line width) is bigger than container width, so wrap the text (by removing previous nowrap text-wrap property) and calculate parent div width to fit the wrapped text max width
+                        else {
+                            spanTextNode.style.removeProperty("text-wrap")
+                            pn.style.width = (spanTextNode.offsetWidth + pnStyleMap.get('padding-left')['value'] + pnStyleMap.get('padding-right')['value']) + 'px'
+                        }
+                    }
+                )
+            }
+        )
+        skillsItemResizeObserver.observe(descGridNode)
+    }
+)
