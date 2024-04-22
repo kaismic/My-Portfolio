6c0502769dc97b0788d3490e5bb5465e58610d7f
Isac Kim <isac08270@gmail.com>
2024-04-23 02:46:22 +1000

    fixed skills item width adjusting code

--- src/main.js
+++ src/main.js
@@ -22,13 +22,22 @@ Array.from(document.getElementsByClassName('embla__carousel')).map(
     }
 )
 
-let count = 0
-
-Array.from(document.getElementsByClassName('project__desc-grid')).map(
-    (descGridNode, idx, arr) => {
-        const skillsItemResizeObserver = new ResizeObserver(
+Array.from(document.getElementsByClassName('project__card')).map(
+    (projectCard, idx, arr) => {
+        // need to observe project__desc-grid indirectly through embla__carousel because it changes width when screen width is changed but does not change height when skills items heights are changed so it doesn't cause infinite resize observer call
+        const carousel = projectCard.querySelector('.embla__carousel')
+        const carouselResizeObs = new ResizeObserver(
             function (entries, observer) {
-                const skillsContainerWidth = descGridNode.querySelector('.project__skills-container').offsetWidth
+                const descGridNode = projectCard.querySelector('.project__desc-grid')
+                if (descGridNode.isCalculatingWidth) {
+                    console.log("returned from resize observer")
+                    return
+                }
+                descGridNode.isCalculatingWidth = true
+                const descGridNodeWidth = descGridNode.offsetWidth
+                const skillsTextWidth = descGridNode.children[2].offsetWidth
+                const columnGap = descGridNode.computedStyleMap().get('column-gap')['value']
+                const skillsContainerWidth = descGridNodeWidth - skillsTextWidth - columnGap
                 Array.from(descGridNode.getElementsByClassName('project__skills-item')).map(
                     /**
                      * 
@@ -37,25 +46,28 @@ Array.from(document.getElementsByClassName('project__desc-grid')).map(
                      * @param {Array<HTMLElement>} arr 
                      */
                     (spanTextNode, idx, arr) => {
-                        spanTextNode.style.textWrap = "nowrap"
                         /**
                          * @type {HTMLDivElement}
                          */
+                        spanTextNode.style.textWrap = "nowrap"
                         const pn = spanTextNode.parentNode
                         const pnStyleMap = pn.computedStyleMap()
-                        // if the text full width is less than container width, keep text-wrap nowrap and remove width property in case it was assigned before
-                        if (spanTextNode.offsetWidth <= skillsContainerWidth) {
+                        // if the text single line width (including padding) is less than container width, keep text-wrap nowrap and remove width property in case it was assigned before
+                        const pnWidth = pn.offsetWidth
+                        console.log("pnWidth =",pnWidth)
+                        if (pnWidth <= skillsContainerWidth) {
                             pn.style.removeProperty("width")
                         }
                         // the text full width (single line width) is bigger than container width, so wrap the text (by removing previous nowrap text-wrap property) and calculate parent div width to fit the wrapped text max width
                         else {
                             spanTextNode.style.removeProperty("text-wrap")
-                            pn.style.width = (spanTextNode.offsetWidth + pnStyleMap.get('padding-left')['value'] + pnStyleMap.get('padding-right')['value']) + 'px'
+                            pn.style.width = (spanTextNode.offsetWidth + pnStyleMap.get('padding-left')['value'] + pnStyleMap.get('padding-right')['value'] + 2 * pnStyleMap.get('border-left-width')['value']) + 'px'
                         }
                     }
                 )
+                descGridNode.isCalculatingWidth = false
             }
         )
-        skillsItemResizeObserver.observe(descGridNode)
+        carouselResizeObs.observe(carousel)
     }
 )
