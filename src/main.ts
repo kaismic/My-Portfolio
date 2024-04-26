501c161b06223462376f87c889be4bddceadb53f
Isac Kim <isac08270@gmail.com>
2024-04-26 20:00:44 +1000

    added hitomi scroll viewer project

--- src/main.ts
+++ src/main.ts
@@ -55,11 +55,39 @@ const projectCardInfos: ProjectCardInfo[] = [
             }
         ],
         paragraphs: [
-            "<b>An Android application which allows customising keyboard layouts</b> developed using <span class='text-pink-500'>Kotlin</span> and <span class='text-pink-500'>Android Studio</span>. This keyboard application was developed because I could not find a decent keyboard application which also supports keyboard layout customisation.",
+            "<b>An Android application which allows customising keyboard layouts</b> developed using <span class='text-pink-500'>Kotlin</span> and <span class='text-pink-500'>Android Studio</span>. This application was developed because I could not find a decent keyboard application which also supports keyboard layout customisation.",
             "One of the valuable experiences that I have gained while working on this project was the experience of using features of <span class='text-pink-500'>Object Oriented Programming</span>. As an example, during development, I realised that I was writing overlapping codes repeatedly, and this was when the concept of <span class='text-pink-500'>inheritance</span> came to my mind. Using inheritance effectively reduced the amount of code I had to write and improved reusability of the code. Furthermore, I have also gained hands-on experience on Kotlin and android application lifecycle while working on this project."
         ],
         githubLink: "https://github.com/kaismic/My-Portfolio/",
         skills: ["Kotlin", "Android", "Android Studio"]
+    },
+    {
+        title: "Hitomi Scroll Viewer",
+        subtitle: "Jul 2022 - Mar 2024 (Solo Project)",
+        slides: [
+            {
+                tag: "img",
+                src: "./images/hitomi-scroll-viewer/preview_0.png"
+            },
+            {
+                tag: "img",
+                src: "./images/hitomi-scroll-viewer/preview_1.png"
+            },
+            {
+                tag: "img",
+                src: "./images/hitomi-scroll-viewer/preview_2.png"
+            },
+            {
+                tag: "img",
+                src: "./images/hitomi-scroll-viewer/preview_3.png"
+            }
+        ],
+        paragraphs: [
+            "<b>A Windows application manga viewer with additional features such as auto scrolling, searching by tags and downloading.</b> This application was developed because the original website lacked functionalities that I needed and it was inconvenient to use the already existing features in the website. Developed using <span class='text-pink-500'>C#</span>, <span class='text-pink-500'>.NET</span> and <span class='text-pink-500'>WinUI 3</span> Platform",
+            "mention utilising mutex, multi threading i guess?, modularising components, etc."
+        ],
+        githubLink: "https://github.com/kaismic/Hitomi-Scroll-Viewer",
+        skills: ["C#", ".NET", "WinUI 3", "Windows"]
     }
 ]
 
