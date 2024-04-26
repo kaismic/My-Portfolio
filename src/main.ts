e5edc3cdec3515d08e5d94a254f625cbfc18a4df
Isac Kim <isac08270@gmail.com>
2024-04-26 20:04:00 +1000

    changed projects order and fixed portfolio date in subtitle

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { ProjectCard, ProjectCardInfo } from './components/ProjectCard'
 const projectCardInfos: ProjectCardInfo[] = [
     {
         title: "Portfolio Website",
-        subtitle: "Apr 2022 - Work in Progress (Solo Project)",
+        subtitle: "Apr 2024 - Work in Progress (Solo Project)",
         slides: [
             {
                 tag: "img",
@@ -34,34 +34,6 @@ const projectCardInfos: ProjectCardInfo[] = [
         ]
     },
     {
-        title: "Custom Layout Keyboard for Android",
-        subtitle: "Apr 2022 - Dec 2023 (Solo Project)",
-        slides: [
-            {
-                tag: "video",
-                src: "./images/custom-layout-keyboard/screen_record_0.mp4"
-            },
-            {
-                tag: "img",
-                src: "./images/custom-layout-keyboard/screenshot_0.png"
-            },
-            {
-                tag: "img",
-                src: "./images/custom-layout-keyboard/screenshot_1.png"
-            },
-            {
-                tag: "img",
-                src: "./images/custom-layout-keyboard/screenshot_2.png"
-            }
-        ],
-        paragraphs: [
-            "<b>An Android application which allows customising keyboard layouts</b> developed using <span class='text-pink-500'>Kotlin</span> and <span class='text-pink-500'>Android Studio</span>. This application was developed because I could not find a decent keyboard application which also supports keyboard layout customisation.",
-            "One of the valuable experiences that I have gained while working on this project was the experience of using features of <span class='text-pink-500'>Object Oriented Programming</span>. As an example, during development, I realised that I was writing overlapping codes repeatedly, and this was when the concept of <span class='text-pink-500'>inheritance</span> came to my mind. Using inheritance effectively reduced the amount of code I had to write and improved reusability of the code. Furthermore, I have also gained hands-on experience on Kotlin and android application lifecycle while working on this project."
-        ],
-        githubLink: "https://github.com/kaismic/My-Portfolio/",
-        skills: ["Kotlin", "Android", "Android Studio"]
-    },
-    {
         title: "Hitomi Scroll Viewer",
         subtitle: "Jul 2022 - Mar 2024 (Solo Project)",
         slides: [
@@ -88,6 +60,34 @@ const projectCardInfos: ProjectCardInfo[] = [
         ],
         githubLink: "https://github.com/kaismic/Hitomi-Scroll-Viewer",
         skills: ["C#", ".NET", "WinUI 3", "Windows"]
+    },
+    {
+        title: "Custom Layout Keyboard for Android",
+        subtitle: "Apr 2022 - Dec 2023 (Solo Project)",
+        slides: [
+            {
+                tag: "video",
+                src: "./images/custom-layout-keyboard/screen_record_0.mp4"
+            },
+            {
+                tag: "img",
+                src: "./images/custom-layout-keyboard/screenshot_0.png"
+            },
+            {
+                tag: "img",
+                src: "./images/custom-layout-keyboard/screenshot_1.png"
+            },
+            {
+                tag: "img",
+                src: "./images/custom-layout-keyboard/screenshot_2.png"
+            }
+        ],
+        paragraphs: [
+            "<b>An Android application which allows customising keyboard layouts</b> developed using <span class='text-pink-500'>Kotlin</span> and <span class='text-pink-500'>Android Studio</span>. This application was developed because I could not find a decent keyboard application which also supports keyboard layout customisation.",
+            "One of the valuable experiences that I have gained while working on this project was the experience of using features of <span class='text-pink-500'>Object Oriented Programming</span>. As an example, during development, I realised that I was writing overlapping codes repeatedly, and this was when the concept of <span class='text-pink-500'>inheritance</span> came to my mind. Using inheritance effectively reduced the amount of code I had to write and improved reusability of the code. Furthermore, I have also gained hands-on experience on Kotlin and android application lifecycle while working on this project."
+        ],
+        githubLink: "https://github.com/kaismic/My-Portfolio/",
+        skills: ["Kotlin", "Android", "Android Studio"]
     }
 ]
 
